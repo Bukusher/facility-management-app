e60a64842e2c3ac7fa684f7e806936ad296ada07
Stiv <Stiv.abdullwahed0007@stud.hkr.se>
2019-05-02 22:10:34 +0200

    scene switching methods

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Scene1Login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
     }
 
