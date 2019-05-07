6fe1e697011cd29becab65e9878f5f6a9bc82ea7
bukusher <alejandro.buchacher@gmail.com>
2019-05-07 12:26:21 +0200

    refactored scenes into another package

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Scene1Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../scenes/Scene1Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
