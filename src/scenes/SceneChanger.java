5411a9d60c3011b317dc9111388bb84b642b718c
jonasjoh97 <jonas.johansson0280@stud.hkr.se>
2019-05-09 13:10:26 +0200

    Added a class and method to change scenes easier.

---
+++ src/scenes/SceneChanger.java
@@ -0,0 +1,21 @@
+package scenes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneChanger {
+
+    public void SceneChange (ActionEvent event, String file) throws IOException {
+        Parent homePageParent = FXMLLoader.load(getClass().getResource(file));
+        Scene homePageScene = new Scene(homePageParent);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setScene(homePageScene);
+        appStage.show();
+    }
+}
