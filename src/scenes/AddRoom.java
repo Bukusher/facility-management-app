6261596e4047d5873a5f8cdf9e047de96f9546aa
Stiv <Stiv.abdullwahed0007@stud.hkr.se>
2019-05-14 13:45:05 +0200

    put variables for create room scene checkboxes and textfields

--- src/scenes/AddRoom.java
+++ src/scenes/AddRoom.java
@@ -4,19 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class AddRoom {
     SceneChanger sceneChange = new SceneChanger();
 
     @FXML
-    TextField TFnewroomname;
+    TextField TFnewNameAdd;
     @FXML
-    TextField TFnewchairamount;
+    TextField TFnewChairAdd;
     @FXML
-    TextField TFnewroomsize;
+    TextField TFnewSizeAdd;
     @FXML
     CheckBox CBtv;
     @FXML
@@ -37,6 +37,41 @@ public class AddRoom {
         sceneChange.SceneChange(event, "Scene2Dashboard.fxml");
     }
 
+@FXML
+    private void AddRoom(ActionEvent event) throws IOException{
+      byte tv;
+      byte projector;
+      byte whiteboard;
+      byte sink;
+      byte microphone;
+      byte speakers;
+      byte overheadprojector;
+     String nameadd = TFnewNameAdd.getText();
+     String chairadd=TFnewChairAdd.getText();
+     String size=TFnewSizeAdd.getText();
+        if (CBtv.isSelected()){
+         tv=1;
+      }else tv=0;
+    if (CBprojector.isSelected()){
+        projector=1;
+    }else projector=0;
+    if (CBwhiteboard.isSelected()){
+        whiteboard=1;
+    }else whiteboard=0;
+    if (CBsink.isSelected()){
+        sink=1;
+    }else sink=0;
+    if (CBmicrophone.isSelected()){
+        microphone=1;
+    }else microphone=0;
+    if (CBspeakers.isSelected()){
+        speakers=1;
+    }else speakers=0;
+    if (CBoverheadprojector.isSelected()){
+        overheadprojector=1;
+    }else overheadprojector=0;
+    //Putting all vaules in variables
+    }
 
 
 
