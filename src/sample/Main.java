e7c1e3f3fe94d73ebdc68a58d6b266c1a377acb2
Stiv <Stiv.abdullwahed0007@stud.hkr.se>
2019-05-21 21:11:21 +0200

    Email Methods implemented

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         DB_Connector db_connector = new DB_Connector();
         db_connector.connect();
+
         launch(args);
 
 
